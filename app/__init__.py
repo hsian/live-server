1a1e6de01321df5f7fa48b24fac048c8dd8ff121
itcast-hsian <541510140@qq.com>
2020-06-12 17:42:45 +0800

    .

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from config import config
 
 def create_app(config_name):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../client')
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     CORS(app, resources={r"/api/*": {"origins": "*"}})

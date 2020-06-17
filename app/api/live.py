c50895be7ad36066693100e34038a5917c88ca8b
itcast-hsian <541510140@qq.com>
2020-06-17 11:32:39 +0800

    .

--- app/api/live.py
+++ app/api/live.py
@@ -1,12 +1,19 @@
 from flask import jsonify, request, g
+from urllib import parse
 from . import api
 
-@api.route('/test/', methods=['get','post'])
+@api.route('/client_connect/', methods=['get','post'])
 def test():
-    print('444')
+    # pass
     return '0'
 
-@api.route('/client_connect/', methods=['get','post'])
+@api.route('/client_publish/', methods=['get','post'])
 def client_connect():
-    print('123123123123123')
+    url = request.json.get("param")
+    result = parse.urlparse(url)
+    query_dict = parse.parse_qs(result.query)
+    token = query_dict.get('token', [None])[0]
+    if (token is None) and (token != '123456'):
+        return '1'
     return '0'
+
